1ecc10aece9451970d246d22e63c711035230a2a
Jeral Pineda <jeral.cris@gmail.com>
2026-06-02 20:52:41 -0600

    refactor: upgrade formatting for astro

--- .config/nvim/lua/jeral/plugins/formatting.lua
+++ .config/nvim/lua/jeral/plugins/formatting.lua
@@ -20,7 +20,7 @@ return {
         liquid = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
-        astro = { "astro" },
+        astro = { "prettier" },
       },
       format_on_save = {
         lsp_fallback = true,
